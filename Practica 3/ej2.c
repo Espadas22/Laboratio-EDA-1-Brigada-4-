101fa9577ebfec42fbf164999050b7227a5a78a3
Anthony_Espadas <anthony.espadas@ingenieria.unam.edu>
2021-03-26 19:58:04 -0600

    Carga de archivos practica 3

--- Practica 3/ej2.c
+++ Practica 3/ej2.c
@@ -12,13 +12,15 @@ int main()
     POLINOMIO* Polinomio_1 = Crear_polinomio();
     printf("\nEl primer polinomio es:\n");
     Imprimir_polinomio(Polinomio_1);
+    printf("\n");
 
     POLINOMIO* Polinomio_2 = Crear_polinomio();
     printf("\nEl segundo polinomio es:\n");
     Imprimir_polinomio(Polinomio_2);
+    printf("\n");
 
-    POLINOMIO* Resultado = Sumar_polinomios(Polinomio_1, Polinomio_2);
-    printf("\nY su suma es:\n");
+    POLINOMIO* Resultado = Restar_polinomios(Polinomio_1, Polinomio_2);
+    printf("\nY su resta es:\n");
     Imprimir_polinomio(Resultado);
     
     Liberar_polinomio(Polinomio_1);
@@ -67,9 +69,15 @@ void Imprimir_polinomio(POLINOMIO* Polinomio)
                 printf("+ %d\n", Polinomio -> Terminos -> Coeficiente);
         else
             if (Polinomio -> Terminos -> Coeficiente < 0) // Para los terminos negativos
-                printf("- %dx^%d ", (-1) * Polinomio -> Terminos -> Coeficiente, Polinomio -> Terminos -> Exponente);
+                if (Polinomio -> Terminos -> Coeficiente == -1) // Si el coeficiente es 1 no es necesario escribirlo
+                    printf("- x^%d ", Polinomio -> Terminos -> Exponente);
+                else
+                    printf("- %dx^%d ", (-1) * Polinomio -> Terminos -> Coeficiente, Polinomio -> Terminos -> Exponente);
             else // Para los positivos
-                printf("+ %dx^%d ", Polinomio -> Terminos -> Coeficiente, Polinomio -> Terminos -> Exponente);
+                if (Polinomio -> Terminos -> Coeficiente == 1) // Si el coeficiente es 1 no es necesario escribirlo
+                    printf("+ x^%d ", Polinomio -> Terminos -> Exponente);
+                else
+                    printf("+ %dx^%d ", Polinomio -> Terminos -> Coeficiente, Polinomio -> Terminos -> Exponente);
     }
 
     Polinomio -> Terminos -= Polinomio -> Grado + 1; // Devolvemos al apuntador a su posision original
@@ -126,9 +134,55 @@ POLINOMIO* Sumar_polinomios(POLINOMIO* Polinomio_1, POLINOMIO* Polinomio_2)
     return Resultado;
 }
 
-POLINOMIO* Restar_polinomios(POLINOMIO* Polinomio_1, POLINOMIO* Polinomio_2)
+POLINOMIO* Restar_polinomios(POLINOMIO* Polinomio_1, POLINOMIO* Polinomio_2) // El polinomio 2 siempre es el que se va a restar
 {
+    POLINOMIO* Resultado = (POLINOMIO*) malloc(sizeof(POLINOMIO)); // Reservamos memoria para el resultado
+    
+    // Reservamos memoria para el resultado segun el polinomio de mayor grado
+    if (Polinomio_1 -> Grado >= Polinomio_2 -> Grado) // Casos donde el uno es mayor o son iguales
+    {
+        Resultado -> Grado = Polinomio_1 -> Grado;
+        Resultado -> Terminos = (TERMINO*) malloc ((Polinomio_1 -> Grado + 1)*(sizeof(TERMINO)));
 
+        // Realizamos las operaciones de asignacion
+        for (int i = Resultado -> Grado; i >= 0; i--, Polinomio_1 -> Terminos++, Resultado -> Terminos++)
+        {
+            if (i > Polinomio_2 -> Grado) // Para las asignaciones de exponente donde el Polonomio 2 no tiene termino
+                Resultado -> Terminos -> Coeficiente = Polinomio_1 -> Terminos -> Coeficiente;
+            else 
+            {
+                Resultado -> Terminos -> Coeficiente = Polinomio_1 -> Terminos -> Coeficiente - Polinomio_2 -> Terminos -> Coeficiente;
+                Polinomio_2 -> Terminos++;
+            }
+
+            Resultado -> Terminos ->Exponente = i;
+        }
+    }
+    else // Casos para cuando el 2 es mayor
+    {
+        Resultado -> Grado = Polinomio_2 -> Grado;
+        Resultado -> Terminos = (TERMINO*) malloc ((Polinomio_2 -> Grado + 1)*(sizeof(TERMINO)));
+
+        for (int i = Resultado -> Grado; i >= 0; i--, Polinomio_2 -> Terminos++, Resultado -> Terminos++)
+        {
+            if (i > Polinomio_1 -> Grado) // Para las asignaciones de exponente donde el Polonomio 1 no tiene termino
+                Resultado -> Terminos -> Coeficiente = (-1) * Polinomio_2 -> Terminos -> Coeficiente; // Se invierte su valor al ser una resta
+            else
+            {
+                Resultado -> Terminos -> Coeficiente = Polinomio_1 -> Terminos -> Coeficiente - Polinomio_2 -> Terminos -> Coeficiente;
+                Polinomio_1 -> Terminos++;
+            }
+
+            Resultado -> Terminos -> Exponente = i;
+        }
+    }
+
+    // Devolvemos los apuntadores de cada polinomio a la posicion inicial
+    Polinomio_1 -> Terminos -= Polinomio_1 -> Grado + 1;
+    Polinomio_2 -> Terminos -= Polinomio_2 -> Grado + 1;
+    Resultado -> Terminos -= Resultado -> Grado + 1;
+
+    return Resultado;
 }
 
 void Liberar_polinomio(POLINOMIO* Polinomio)
